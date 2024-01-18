7c91785d0431248591010067b239b6c1e2bb117b
Naum Santos Mourão <naum.santos@unemat.br>
2024-01-18 03:39:44 -0300

    :sparkles: feat: Criando seção de projetos

--- src/app/components/Projects/Projects.js
+++ src/app/components/Projects/Projects.js
@@ -1,24 +1,81 @@
+import Image from 'next/image';
 import styles from './Projects.module.css';
 
 const Projects = () => {
   return (
-    <div>
-      <section id={styles.projects}>
-        <h2>Projetos</h2>
-        <div className={styles.grid}>
-          <a
-            href="#"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2>
-              Weather Forecast <span>-&gt;</span>
-            </h2>
+    <div className={styles.projects}>
+      <h2>Projetos</h2>
+      <div className={styles.grid}>
+        <div className={styles.grid_card}>
+          <div>
+            <a
+              href="https://github.com/Naumsantos/weather"
+              className={styles.card}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <h3>
+                Weather Forecast <span>-&gt;</span>
+              </h3>
+            </a>
             <p>Projeto de previsão de tempo.</p>
-          </a>
+          </div>
+
+          <Image
+            src="/images/imageProjects/weatherForecast.png"
+            alt='imagem do aplicatico de previsão do tempo'
+            width={400}
+            height={300}
+            className={styles.image_card}
+          />
         </div>
-      </section>
+        <div className={styles.grid_card}>
+          <div>
+            <a
+              href="https://github.com/Naumsantos/weather"
+              className={styles.card}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <h3>
+                Weather Forecast <span>-&gt;</span>
+              </h3>
+            </a>
+            <p>Projeto de previsão de tempo.</p>
+          </div>
+
+          <Image
+            src="/images/imageProjects/weatherForecast.png"
+            alt='imagem do aplicatico de previsão do tempo'
+            width={400}
+            height={300}
+            className={styles.image_card}
+          />
+        </div>
+        <div className={styles.grid_card}>
+          <div>
+            <a
+              href="https://github.com/Naumsantos/weather"
+              className={styles.card}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <h3>
+                Weather Forecast <span>-&gt;</span>
+              </h3>
+            </a>
+            <p>Projeto de previsão de tempo.</p>
+          </div>
+
+          <Image
+            src="/images/imageProjects/weatherForecast.png"
+            alt='imagem do aplicatico de previsão do tempo'
+            width={400}
+            height={300}
+            className={styles.image_card}
+          />
+        </div>
+      </div>
     </div>
   )
 }
